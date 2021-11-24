dcacfbf58f5128bde978f15ea4c397493c681833
fronte-finem <fronte.finem@yandex.ru>
2021-11-24 23:18:40 +0300

    feat: add type for person

---
+++ src/types/person.js
@@ -0,0 +1,7 @@
+/**
+ * @typedef { Object } Person
+ * @property { string } [id] - unique identifier (`string`, `uuid`) generated on server side
+ * @property { string } name - person's name (`string`, **required**)
+ * @property { number } age - person's age (`number`, **required**)
+ * @property { string[] } hobbies - person's hobbies (`array` of `strings` or empty `array`, **required**)
+ */
