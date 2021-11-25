3ea526f72eb4a4b5d5cb1f33eb69332f32082df9
fronte-finem <fronte.finem@yandex.ru>
2021-11-26 00:41:02 +0300

    refactor: add http-methods switch to person controller

--- src/app/person/controller.js
+++ src/app/person/controller.js
@@ -1,12 +1,21 @@
+import { Method } from '../../constants/http.js';
+import { handlerGet } from './handler-get.js';
+import { HttpError } from '../../errors/http-error.js';
+
 /**
  * @type { Controller }
  */
-export const personController = (path, req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  res.statusCode = 200;
-  res.end(
-    JSON.stringify({
-      data: 'Hello World!',
-    })
-  );
+export const personController = async (path, request, response) => {
+  switch (request.method) {
+    case Method.GET:
+      return handlerGet(path, request, response);
+    case Method.POST:
+      return handlerGet(path, request, response);
+    case Method.PUT:
+      return handlerGet(path, request, response);
+    case Method.DELETE:
+      return handlerGet(path, request, response);
+    default:
+  }
+  throw new HttpError(500);
 };
