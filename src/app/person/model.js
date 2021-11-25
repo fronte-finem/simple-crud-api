6b3ab440a301a05569b9d55edeccda51056d383b
fronte-finem <fronte.finem@yandex.ru>
2021-11-26 00:35:41 +0300

    refactor: update person model types

--- src/app/person/model.js
+++ src/app/person/model.js
@@ -1,7 +1,19 @@
 /**
  * @typedef { Object } Person
- * @property { string } [id] - unique identifier (`string`, `uuid`) generated on server side
  * @property { string } name - person's name (`string`, **required**)
  * @property { number } age - person's age (`number`, **required**)
  * @property { string[] } hobbies - person's hobbies (`array` of `strings` or empty `array`, **required**)
+ */
+
+/**
+ * @typedef { Object } DatabaseItem
+ * @property { string } id - unique identifier (`string`, `uuid`) generated on server side
+ */
+
+/**
+ * @typedef { DatabaseItem & Person } PersonDatabaseItem
+ */
+
+/**
+ * @typedef { PersonDatabaseItem | undefined } MaybePersonDatabaseItem
  */
