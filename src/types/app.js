e34fb851873cec770aaeb79c8949ca09354ca901
fronte-finem <fronte.finem@yandex.ru>
2021-11-26 00:35:09 +0300

    refactor: update app types

--- src/types/app.js
+++ src/types/app.js
@@ -1,12 +1,20 @@
 /**
- * @typedef { import('http').IncomingMessage } AppRequest
- * @typedef { import('http').ServerResponse } AppResponse
+ * @typedef { module:http.IncomingMessage } AppRequest
+ * @typedef { module:http.ServerResponse } AppResponse
  */
 
 /**
  * @callback Controller
  * @param { string[] } path
- * @param { Request } req
- * @param { Response } res
+ * @param { AppRequest } request
+ * @param { AppResponse } response
+ * @return { Promise<void> }
+ */
+
+/**
+ * @callback Middleware
+ * @param { AppRequest } request
+ * @param { AppResponse } response
+ * @param { Middleware } [next]
  * @return { void }
  */
