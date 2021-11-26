dcec45467d8b752ae7cdb12d4e72e8033c15708c
fronte-finem <fronte.finem@yandex.ru>
2021-11-26 15:38:52 +0300

    refactor: make implementation more safe and flexible

--- src/errors/http-error.js
+++ src/errors/http-error.js
@@ -1,23 +1,36 @@
 import http from 'http';
-import { stringify } from '../utils/stringify.js';
 
-export class HttpError extends Error {
-  constructor(code) {
-    super(http.STATUS_CODES[code]);
-    this.code = code;
-  }
-}
+const DEFAULT_CODE = 500;
+const DEFAULT_MESSAGE = http.STATUS_CODES[DEFAULT_CODE];
 
 /**
- * @param { * } unknownError
- * @return  { HttpError }
+ * @param { number } code
+ * @return { string }
  */
-export function getHttpError(unknownError) {
-  if (unknownError instanceof HttpError) {
-    return unknownError;
+const unknownCodeMessage = (code) =>
+  `${DEFAULT_MESSAGE} [unknown code ${code}]`;
+
+/**
+ * @param { string } statusMessage
+ * @param { string } message
+ * @return { string }
+ */
+const composeMessage = (statusMessage, message) =>
+  `${statusMessage}${message ? ` — ${message}` : ''}`;
+
+export class HttpError extends Error {
+  /**
+   * @description Pass code from {@link module:http.STATUS_CODES}
+   * @requires module:http.STATUS_CODES
+   * @param { number } code
+   * @param { string } [message]
+   */
+  constructor(code, message) {
+    const maybeStatusMessage = http.STATUS_CODES[code];
+    const safeCode = maybeStatusMessage ? code : DEFAULT_CODE;
+    const safeStatusMessage = maybeStatusMessage || unknownCodeMessage(code);
+    const safeMessage = composeMessage(safeStatusMessage, message);
+    super(safeMessage);
+    this.code = safeCode;
   }
-  if (typeof unknownError.message === 'string') {
-    return new HttpError(500, unknownError.message);
-  }
-  return new HttpError(500, stringify(unknownError));
 }
