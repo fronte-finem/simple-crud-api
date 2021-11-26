1bb2e9ab867df2173b3b62dad4fad9163108891a
fronte-finem <fronte.finem@yandex.ru>
2021-11-26 15:46:57 +0300

    refactor: add handle-create to person controller

--- src/app/person/controller.js
+++ src/app/person/controller.js
@@ -1,6 +1,7 @@
 import { Method } from '../../constants/http.js';
-import { handlerGet } from './handler-get.js';
+import { handlerRead } from './handler-read.js';
 import { HttpError } from '../../errors/http-error.js';
+import { handlerCreate } from './handler-create.js';
 
 /**
  * @type { Controller }
@@ -8,13 +9,13 @@ import { HttpError } from '../../errors/http-error.js';
 export const personController = async (path, request, response) => {
   switch (request.method) {
     case Method.GET:
-      return handlerGet(path, request, response);
+      return handlerRead(path, request, response);
     case Method.POST:
-      return handlerGet(path, request, response);
+      return handlerCreate(path, request, response);
     case Method.PUT:
-      return handlerGet(path, request, response);
+      return handlerRead(path, request, response);
     case Method.DELETE:
-      return handlerGet(path, request, response);
+      return handlerRead(path, request, response);
     default:
   }
   throw new HttpError(500);
