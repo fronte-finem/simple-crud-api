5939b395a09c6a600c7c11d81c057acbb1d8ad38
fronte-finem <fronte.finem@yandex.ru>
2021-11-26 15:07:15 +0300

    feat: add bad request error

---
+++ src/errors/http-error-bad-request.js
@@ -0,0 +1,11 @@
+import { HttpError } from './http-error.js';
+
+export class HttpErrorBadRequest extends HttpError {
+  /**
+   * @param { string } message
+   */
+  constructor(message) {
+    super(400);
+    this.message = `${this.message} — ${message}`;
+  }
+}
