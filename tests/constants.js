df0dacfe99c8fd86ba485f47de5dea1c28c11fa5
fronte-finem <fronte.finem@yandex.ru>
2021-11-28 21:31:17 +0300

    test: add some constants

---
+++ tests/constants.js
@@ -0,0 +1,8 @@
+import { makePerson } from './helpers.js';
+
+export const ROUTE = '/person';
+
+export const UUID_LENGTH = 36;
+
+export const PERSON_1 = makePerson('Qwerty', 23, [...'🙈🙉🙊🐵']);
+export const PERSON_2 = makePerson('Йцукен', 45, [...'🍷🍸🍹🍺🥃']);
